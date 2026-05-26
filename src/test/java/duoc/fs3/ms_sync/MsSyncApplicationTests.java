ba6783157c77c3aa343a117addcac6b7dd8706c5
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-26 09:28:17 -0400

    iniciando backend

---
+++ src/test/java/duoc/fs3/ms_sync/MsSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package duoc.fs3.ms_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
