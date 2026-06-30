788a92cba456c42d0a013dd1512611ca252f6639
BrazenGhost630 <fel.vargaso@duocuc.cl>
2026-06-29 22:33:02 -0400

    Exclude SecurityAutoConfiguration from Spring Boot app

--- src/main/java/duoc/fs3/ms_sync/MsSyncApplication.java
+++ src/main/java/duoc/fs3/ms_sync/MsSyncApplication.java
@@ -3,7 +3,7 @@ package duoc.fs3.ms_sync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MsSyncApplication {
 
 	public static void main(String[] args) {
